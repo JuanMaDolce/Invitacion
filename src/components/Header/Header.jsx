f9d554a944fe8b57823761f1fa10b3e45996880f
Juan Manuel Dolce <jmdolce@hotmail.com>
2024-09-02 21:21:45 -0300

    7 commit

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -3,8 +3,10 @@ import '../../styles/Header.css'
 const Header = () => {
     return(
         <div className='header'>
-            <h1>Ceci & Juanma</h1>
-            <h2>Nuestra Boda</h2>
+            <div>
+                <h1>Ceci & Juanma</h1>
+            </div>
+            <h2>Nos Casamos!</h2>
         </div>
     )
 }
